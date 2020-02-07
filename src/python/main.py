4e7701ee7719769a23b175701304f8b1f1f803b3
Brian Edwards <bedwards@indeed.com>
2020-02-06 21:10:25 -0600

    SSE

--- src/python/main.py
+++ src/python/main.py
@@ -1,6 +1,36 @@
+from flask import Flask, stream_with_context, request, Response
+from datetime import datetime
+import time
+
+app = Flask(__name__)
+
+@app.route("/streams")
+def stream():
+    def event_stream():
+        while True:
+            time.sleep( 5 )
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            yield "data: {}\n\n".format(current_time)
+    
+    return Response(event_stream(), mimetype="text/event-stream")
+
+@app.route('/stream')
+def streamed_response():
+    def generate():
+        yield 'Hello '
+        yield request.args['name']
+        yield '!'
+    return Response(stream_with_context(generate()), mimetype="text/event-stream")
+
+@app.route('/')
+def hello_world():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return 'Hello, World {}'.format(current_time)
 
 def main():
-    print("python main function")
+    app.run(host='0.0.0.0', port=9876)
 
 if __name__ == '__main__':
     main()
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.join(__dirname, 'src', 'javascript', 'app.js'), 
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
+  entry: path.join(__dirname, 'src', 'javascript', 'app.js'),
+  output: {
+    path: path.join(__dirname, 'public', 'js'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader',
-          ],
-        },
-      ]
-    }
-  };
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
